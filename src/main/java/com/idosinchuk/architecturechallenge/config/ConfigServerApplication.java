e7f264bf44a35fb2466c6ffd05ca6ff5fd06328c
idosinchuk <idosinchuk@host.docker.internal>
2019-09-12 13:20:14 +0200

    Refactored for standart

--- src/main/java/com/idosinchuk/architecturechallenge/config/ConfigServerApplication.java
+++ src/main/java/com/idosinchuk/architecturechallenge/config/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.idosinchuk.configserver;
+package com.idosinchuk.architecturechallenge.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerArchitectureChallengeApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerArchitectureChallengeApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
--- src/test/java/com/idosinchuk/configserver/ConfigServerApplication.java
+++ src/test/java/com/idosinchuk/configserver/ConfigServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConfigServerArchitectureChallengeApplicationTests {
+public class ConfigServerApplication {
 
 	@Test
 	public void contextLoads() {
